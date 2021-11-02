f18ee0e9590704c6a82229e287978ec6f94fe649
littleshirleybeans <littleshirleybeans@protonmail.com>
2021-11-02 13:28:47 +0800

    music-player

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "the lakes",
+    artist: "Taylor Swift",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Home",
+    artist: "Bruno Major",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "If I Believe You",
+    artist: "The 1975",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "SLOW DANCING IN THE DARK",
+    artist: "Joji",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Sunsetz",
+    artist: "Cigarettes After Sex",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "I Wanna Be Yours",
+    artist: "Arctic Monkeys",
+    img: "music-6",
+    src: "music-6"
+  }
+]
